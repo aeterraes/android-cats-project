4e3db390a3808bd482b3fe7d22294104d6e1c56d
Veronika <94937684+aeterraes@users.noreply.github.com>
2025-04-05 19:40:29 +0300

    Update ImageRepository.kt

--- app/src/main/java/my/mvp/model/ImageRepository.kt
+++ app/src/main/java/my/mvp/model/ImageRepository.kt
@@ -3,34 +3,8 @@ package my.mvp.model
 import my.mvp.api.ApiService
 import my.mvp.api.ImageResponse
 
-/*
-class ImageRepository(private val context: Context) {
-    fun getCategories(): List<ImageItem> {
-        val assetManager = context.assets
-        val categoryFolders = assetManager.list("categories") ?: return emptyList()
-
-        return categoryFolders.mapNotNull { category ->
-            val images = assetManager.list(
-                "categories/$category") ?: return@mapNotNull null
-            if (images.isNotEmpty()) {
-                ImageItem(
-                    name = category.replace("_", " ")
-                        .replaceFirstChar { if (it.isLowerCase())
-                            it.titlecase(Locale.ROOT) else it.toString() },
-                    path = "categories/$category/${images.first()}",
-                    category = category
-                )
-            } else {
-                null
-            }
-        }
-    }
-}
-
- */
 class ImageRepository(private val apiService: ApiService) {
     suspend fun fetchImages(breedId: String): List<ImageResponse> {
         return apiService.getImagesByBreed(limit = 8, breedId = breedId)
     }
 }
-
